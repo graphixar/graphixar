91c1b6cf923a4b8cbca77e205a1978b8cd111995
Mohammad yarahmadi <yarahmadi.info@gmail.com>
2021-07-25 19:54:00 +0430

    add favicon

--- resources/views/layouts/admin/layout.blade.php
+++ resources/views/layouts/admin/layout.blade.php
@@ -11,7 +11,7 @@
     <meta name="author" content="Graphixar">
     <title>{{ config('app.name') }} | @yield('title', $page_title ?? '')</title>
     <link rel="apple-touch-icon" href="/assets/images/ico/apple-icon-120.png">
-    <link rel="shortcut icon" type="image/x-icon" href="/assets/images/ico/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="media/logo/favicon.png">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;1,400;1,500;1,600" rel="stylesheet">
 
     {{-- Begin Styles --}}
